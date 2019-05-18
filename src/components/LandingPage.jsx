08e11f375ae62f820100a439a886e12a1e9827b2
mattetress <mattetress@me.com>
2019-05-18 14:24:08 -0500

    styling

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,44 +4,54 @@ import { Grid, Cell } from 'react-mdl';
 class Landing extends Component {
   render() {
     return (
-      <div style={{ width: "100%", margin: "auto" }}>
+      <div className='landing-body'>
         <Grid className="landing-grid">
-          <Cell col={12}>
+          <Cell id="img" col={3}>
             <img
               src="/images/IMG_2711.jpg"
               alt="profile"
               className="avatar-image"
+              style={{ height: '150px' }}
             />
-            <div className="banner-text">
-              <h1>Full Stack Web Developer</h1>
-              <hr />
-              <p>Ruby on Rails | JavaScript | React.js | HTML/CSS </p>
+          </Cell>
+          <Cell id="name" col={8}>
+            <div className="name">
+              <h1>Matt Etress</h1>
+              <h2>Full Stack Software Engineer</h2>
+              <section className="icons">
 
-              <div className="social-links">
-
-                {/* LinkedIn */}
-                <a rel="noopener noreferrer" href="http://google.com" target="_blank">
-                  <i className="fa fa-linkedin-square" aria-hidden="true" />
-                </a>
-
-                {/* Github */}
-                <a rel="noopener noreferrer" href="http://google.com" target="_blank">
-                  <i className="fa fa-github-square" aria-hidden="true" />
-                </a>
-
-                {/* Twitter */}
-                <a rel="noopener noreferrer" href="http://google.com" target="_blank">
-                  <i className="fa fa-twitter-square" aria-hidden="true" />
-                </a>
+              </section>
+            </div>
 
 
+          </Cell>
+          {/*
+          <div className="banner-text">
+            <h1>Full Stack Web Developer</h1>
+            <hr />
+            <p>Ruby on Rails | JavaScript | React.js | HTML/CSS </p>
 
-              </div>
+            <div className="social-links">
+
+            <a rel="noopener noreferrer" href="http://google.com" target="_blank">
+              <i className="fa fa-linkedin-square" aria-hidden="true" />
+            </a>
+
+            <a rel="noopener noreferrer" href="http://google.com" target="_blank">
+              <i className="fa fa-github-square" aria-hidden="true" />
+            </a>
+
+            <a rel="noopener noreferrer" href="http://google.com" target="_blank">
+              <i className="fa fa-twitter-square" aria-hidden="true" />
+            </a>
+
+
 
             </div>
-          </Cell>
+
+          </div> */}
         </Grid>
-      </div>
+      </div >
     )
   }
 }
